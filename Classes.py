a7191b2e88b6b2f0e1b5b4c1dc7334b5dfbd531c
jodhvirbassi <jodhvir81@gmail.com>
2023-03-28 21:23:30 -0400

    read cvs file and get attribute function

--- Classes.py
+++ Classes.py
@@ -3,6 +3,7 @@ Project for CSC111
 """
 from __future__ import annotations
 from typing import Any
+import csv
 
 
 class Song:
@@ -21,6 +22,21 @@ class Song:
         self.speech = speech
         self.tempo = tempo
         self.val = valence
+
+    def get_attribute(self):
+        """
+        Return a dict of attributes
+        """
+        attributes = {}
+        attributes['ac'] = self.ac
+        attributes['dance'] = self.dance
+        attributes['energy'] = self.energy
+        attributes['ins'] = self.ins
+        attributes['live'] = self.live
+        attributes['speech'] = self.speech
+        attributes['tempo'] = self.tempo
+        attributes['val'] = self.val
+        return attributes
 
 
 class Graph:
@@ -106,73 +122,27 @@ class Node:
                 vertexes_so_far |= vertex.get_connected_component(visited)
         return vertexes_so_far
 
-# import csv
-#
-#
-# class Song:
-#     """
-#     A class to represent the songs.
-#     """
-#     def __init__(self, id: int, ac: float, dance: float, energy: float,
-#                  ins: float, live: float, speech: float, tempo: float, val: float):
-#         self.id = id
-#         self.ac = ac
-#         self.dance = dance
-#         self.energy = energy
-#         self.ins = ins
-#         self.live = live
-#         self.speech = speech
-#         self.tempo = tempo
-#         self.val = val
-#
-#
-# class Graph:
-#     """
-#     A class to represent all the songs
-#     """
-#     nodes: list[Song]
-#
-#     def __init__(self):
-#         self.nodes = []
-#
-#     def add_song(self, song: Song):
-#         """
-#         Add a song to the graph
-#         """
-#         self.nodes.append(song)
-#
-#     def get_songs(self):
-#         """
-#         Return the songs in this graph
-#         """
-#         return self.nodes
-#
-#     def __len__(self):
-#         return len(self.nodes)
-#
-#
-# def read_csv(csv_file: str):
-#     """
-#     Read the given csv_file and add to the graph
-#     """
-#     graph = Graph()
-#     with open(csv_file) as file:
-#         reader = csv.reader(file)
-#         header = next(file)
-#         for row in reader:
-#             id = int(row[0])
-#             ac = float(row[1])
-#             dance = float(row[2])
-#             energy = float(row[3])
-#             ins = float(row[4])
-#             live = float(row[5])
-#             speech = float(row[6])
-#             tempo = float(row[7])
-#             val = float(row[8])
-#
-#             song = Song(id, ac, dance, energy, ins, live, speech, tempo, val)
-#             graph.add_song(song)
-#     return graph
-#
-#
-# read_csv("test_echo.csv")
+
+def read_csv(csv_file: str):
+    """
+    Read the given csv_file and add to the graph
+    """
+    graph = Graph()
+    with open(csv_file, errors='ignore') as file:
+        reader = csv.reader(file)
+        header = next(file)
+        for row in reader:
+            id = row[0]
+            name = row[1]
+            ac = float(row[12])
+            dance = float(row[6])
+            energy = float(row[7])
+            ins = float(row[13])
+            live = float(row[14])
+            speech = float(row[11])
+            tempo = float(row[15])
+            val = float(row[16])
+
+            song = Song(id, name, ac, dance, energy, ins, live, speech, tempo, val)
+            graph.add_song(song)
+    return graph
